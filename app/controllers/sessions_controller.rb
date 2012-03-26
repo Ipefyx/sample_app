98c75129b57b234345212f038c11823a17fbeb0c
Ipefyx <kernel.panic@hotmail.be>
2012-03-26 21:04:40 +0200

    Use for_tag in session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,14 @@
 class SessionsController < ApplicationController
+	# force_ssl
+
 	def new
 	end
 	
 	def create
-		user = User.find_by_email(params[:session][:email])
-		if user && user.authenticate(params[:session][:password])
+		# user = User.find_by_email(params[:session][:email]) # With form_for
+		# if user && user.authenticate(params[:session][:password])
+		user = User.find_by_email(params[:email]) # With form_tag
+		if user && user.authenticate(params[:password])
 			sign_in user
 			redirect_to(user)
 		else
